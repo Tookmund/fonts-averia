19536983ce82b2b889f26709b72acf486741e297
Jacob Adams <tookmund@gmail.com>
2015-01-23 19:47:21 -0500

    remove example files

--- debian/fonts-averia.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the fonts-averia package
-#
-0 4	* * *	root	[ -x /usr/bin/fonts-averia_maintenance ] && /usr/bin/fonts-averia_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(fonts-averia):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="fonts-averia" command="/usr/bin/fonts-averia"
